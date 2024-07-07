857aefb2dab50e4ce4aa5f8e97c8058162e0cf32
azim0nt <kktotov457@gmail.com>
2024-07-07 17:24:12 +0500

    create update game page

--- backend/netgen/tap_to_earn/urls.py
+++ backend/netgen/tap_to_earn/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', tap_to_earn, name='tap-to-earn'),
-    path('add-game/',add_game, name='add-game')
+    path('add-game/',add_game, name='add-game'),
+    path('update-game/game-<int:pk>',update_game, name='update-game')
 ]
--- backend/netgen/tap_to_earn/views.py
+++ backend/netgen/tap_to_earn/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from .models import Games
 from .forms import GamesForm
 from django.contrib import messages
+
+
 def tap_to_earn(request):
     context = {
-        "games":Games.objects.all()
+        "games": Games.objects.all()
     }
     return render(request, 'tap-to-earn.html', context)
+
 
 def add_game(request):
     form = GamesForm()
@@ -16,5 +19,25 @@ def add_game(request):
             form.save()
             messages.success(request, 'Game added successfully')
             return redirect('tap-to-earn')
-    context = {'form':form}
-    return render(request, 'add_game.html', context) 
+    context = {'form': form}
+    return render(request, 'add_game.html', context)
+
+
+def update_game(request, pk: int):
+    game = Games.objects.get(id=pk)
+    form = GamesForm(instance=game)
+    if request.method == 'POST':
+        form = GamesForm(request.POST)
+        if form.is_valid():
+            game.name = form.cleaned_data.get('name')
+            game.platform = form.cleaned_data.get('platform')
+            game.about = form.cleaned_data.get('about')
+            game.link = form.cleaned_data.get('link')
+            game.save()
+            messages.success(request, 'Game updated successfully')
+            return redirect('tap-to-earn')
+    context = {
+        'form': form,
+        'game': game
+    }
+    return render(request, 'update_game.html', context)
