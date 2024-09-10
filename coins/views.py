a85560407b2e5dbb55ba067b8769be0ea1c1d9e0
azim0nt <kktotov457@gmail.com>
2024-09-10 17:55:55 +0500

    fix small bug

--- coins/views.py
+++ coins/views.py
@@ -1,6 +1,6 @@
 # coins/views.py
 
-from django.shortcuts import render, get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.dateformat import format
 from django.core.serializers.json import DjangoJSONEncoder
 from .models import Coin, CoinPriceHistory
@@ -10,29 +10,33 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext_lazy as _
 
+
 def coin_list(request):
     coins = Coin.objects.all()
     context = {
-        {'coins': coins,
-         "coins_list":_("Список криптовалют"),
-         "add_coin":_("Добавить монету"),
-         "currency":_("Валюта"),
-         "price":_("Цена"),
-         "capitalization":_("Капитализация"),
-         "number_of_coins":_("Количество монет")
-         
-         }
+        'coins': coins,
+        "coins_list": _("Список криптовалют"),
+        "add_coin": _("Добавить монету"),
+        "currency": _("Валюта"),
+        "price": _("Цена"),
+        "capitalization": _("Капитализация"),
+        "number_of_coins": _("Количество монет")
+
+
     }
     return render(request, 'coins.html', context)
 
+
 def coin_detail(request, pk):
     coin = get_object_or_404(Coin, pk=pk)
-    price_history = CoinPriceHistory.objects.filter(coin=coin).order_by('timestamp')
-    prices = [(int(history.timestamp.timestamp() * 1000), float(history.price)) for history in price_history]
-    
+    price_history = CoinPriceHistory.objects.filter(
+        coin=coin).order_by('timestamp')
+    prices = [(int(history.timestamp.timestamp() * 1000),
+               float(history.price)) for history in price_history]
+
     # Convert to JSON with proper formatting
     prices_json = json.dumps(prices, cls=DjangoJSONEncoder)
-    
+
     return render(request, 'coin_detail.html', {'coin': coin, 'prices': prices_json})
 
 
@@ -47,7 +51,8 @@ def add_coin(request):
             messages.success(request, 'Coin added successfully')
             return redirect('tap-to-earn')
         else:
-            messages.error(request, 'There was an error with your form. Please correct the errors and try again.')
+            messages.error(
+                request, 'There was an error with your form. Please correct the errors and try again.')
     else:
         form = CoinsForm()
 
@@ -56,19 +61,19 @@ def add_coin(request):
 
 
 @login_required
-def update_coin(request, pk:int):
+def update_coin(request, pk: int):
     coin = Coin.objects.get(id=pk)
     form = CoinsForm(instance=coin)
     if request.method == 'POST':
-    
+
         form = CoinsForm(request.POST, request.FILES, instance=coin)
-        
+
         if form.is_valid():
             coin.name = form.cleaned_data.get('name')
             coin.symbol = form.cleaned_data.get('symbol')
             coin.market_cap = form.cleaned_data.get('market_cap')
             coin.total_supply = form.cleaned_data.get('total_supply')
-            
+
             coin.save()
             messages.success(request, 'Coin updated successfully')
             return redirect('tap-to-earn')
